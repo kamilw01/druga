264ea10e9c2988cd8f7aa571b4c2c2c087ae6964
kamilw01 <147665208+kamilw01@users.noreply.github.com>
2023-11-04 23:24:23 +0100

    pierwsza zamiana

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace TypeConversion
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            int i = 1000;
+            byte b = (byte)i;
+            Console.WriteLine(b);
+
+        }
+    }
+}
